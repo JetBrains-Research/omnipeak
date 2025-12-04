9a86dc517b3e30b8f0439ab45768501be7f6b65a
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2025-12-04 14:34:28 +0100

    Tabulate ln computations

--- src/main/kotlin/org/jetbrains/bio/omnipeak/statistics/util/PoissonUtil.kt
+++ src/main/kotlin/org/jetbrains/bio/omnipeak/statistics/util/PoissonUtil.kt
@@ -16,7 +16,21 @@ object PoissonUtil {
      * \ln{F(m)} = m*ln{lambda} - \sum_{x=1}^{m}\ln(x)
      * Calculate \ln( sum{exp{N} ) by logspace_add function
      */
-    fun logPoissonCdf(k: Int, lbd: Double, maxM: Int = 10000, epsilon: Double = 1e-5): Double {
+
+    // Tabulating for ln(x) and prefix sums of ln(x)
+    private const val MAX_TAB = 10001
+
+    private val LOGS = DoubleArray(MAX_TAB) { ln(it.toDouble()) }
+
+    private fun lnTab(i: Int): Double = if (i < LOGS.size) LOGS[i] else ln(i.toDouble())
+
+    private val PREFIX_SUMS = DoubleArray(MAX_TAB).apply {
+        for (i in 1 until size) {
+            this[i] = this[i - 1] + LOGS[i]
+        }
+    }
+
+    fun logPoissonCdf(k: Int, lbd: Double, maxM: Int = 10_000, epsilon: Double = 1e-5): Double {
         require(lbd > 0) {
             "Lambda should be > 0, got $lbd"
         }
@@ -25,15 +39,15 @@ object PoissonUtil {
         val lnLbd = ln(lbd)
         // first residue
         val m = k + 1
-        var sumLns = 0.0 // TODO[shpynov] this may be tabulated
-        for (i in 1 until m + 1) {
-            sumLns += ln(i.toDouble())
-        }
+        val sumLns = if (m < PREFIX_SUMS.size)
+            PREFIX_SUMS[m]
+        else
+            PREFIX_SUMS.last() + (PREFIX_SUMS.size .. m).sumOf { lnTab(it) }
         logX = m * lnLbd - sumLns
         residue = logX
         var logy: Double
         for (i in m + 1..maxM) { // Limit
-            logy = logX + lnLbd - ln(i.toDouble())
+            logy = logX + lnLbd - lnTab(i)
             val preResidue = residue
             residue = preResidue logAddExp logy
             if (abs(preResidue - residue) < epsilon)
